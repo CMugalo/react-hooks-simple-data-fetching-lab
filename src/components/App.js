3335f9f903e6b9ce4ae5edea67c1ea0b6546c01e
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-06-05 20:59:14 +0300

    fetched data using useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React from "react";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [displayDog, setDisplayDog] = useState({});
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((dog) => setDisplayDog(dog));
+  }, []);
+
+  if (displayDog.status !== "success") {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <img src={displayDog.message} alt="A Random Dog" />
+    </>
+  );
+}
+export default App;
